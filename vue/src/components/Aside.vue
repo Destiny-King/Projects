793ec8e5bf3d94503d94de7c1b710163dce4266e
Destiny-King <2681283464@qq.com>
2022-06-28 20:23:56 +0800

    add function add delete update query data-display

--- vue/src/components/Aside.vue
+++ vue/src/components/Aside.vue
@@ -3,44 +3,22 @@
     <el-menu
         style="width: 200px;min-height: calc(100vh - 50px)"
         default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose">
+        :default-openeds="['1']"
+        class="el-menu-vertical-demo">
       <el-sub-menu index="1">
-        <template #title>
-          <el-icon>
-            <Location/>
-          </el-icon>
-          <span>导航一</span>
-        </template>
+        <template #title>系统管理</template>
+        <el-menu-item index="user">用户管理</el-menu-item>
       </el-sub-menu>
-      <el-menu-item index="2">
-        <el-icon>
-          <Menu/>
-        </el-icon>
-        <span>导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <el-icon>
-          <Document/>
-        </el-icon>
-        <span>导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <el-icon>
-          <Setting/>
-        </el-icon>
-        <span>导航四</span>
-      </el-menu-item>
     </el-menu>
   </div>
 </template>
 
 <script>
-import { Document, Menu, Location, Setting } from '@element-plus/icons-vue'
+import {Document, Location, Menu, Setting} from '@element-plus/icons-vue'
+
 export default {
   name: "Aside",
-  components: { Document, Menu, Location, Setting }
+  components: {Document, Menu, Location, Setting}
 }
 </script>
 
