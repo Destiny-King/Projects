7c834aee30489d4caf256d87a7cb0ee0511f9e76
Destiny-King <2681283464@qq.com>
2022-11-30 19:23:28 +0800

    Add RemoveElement.

--- Java_Algorithm/Array/src/com/wym/algorithm/MainAlgorithm.java
+++ Java_Algorithm/Array/src/com/wym/algorithm/MainAlgorithm.java
@@ -1,14 +1,22 @@
 package com.wym.algorithm;
 
-import com.wym.algorithm.binarysearch.BinarySearch;
+import com.wym.algorithm.removeelement.RemoveElement;
 
 public class MainAlgorithm {
 
 	public static void main(String[] args) {
-		BinarySearch binarySearch = new BinarySearch();
-		int[] nums = {-1, 0, 3, 5, 9, 12, 14};
-		int target = 9;
-		int ret = binarySearch.search(nums, target);
+		//1、二分查找
+//		BinarySearch binarySearch = new BinarySearch();
+//		int[] nums = {-1, 0, 3, 5, 9, 12, 14};
+//		int target = 9;
+//		int ret = binarySearch.search(nums, target);
+//		System.out.println(ret);
+
+		//2、移除元素
+		RemoveElement removeElement = new RemoveElement();
+		int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+		int val = 2;
+		int ret = removeElement.removeElement(nums, val);
 		System.out.println(ret);
 	}
 }
---
+++ Java_Algorithm/Array/src/com/wym/algorithm/removeelement/RemoveElement.java
@@ -0,0 +1,14 @@
+package com.wym.algorithm.removeelement;
+
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+		int slowIndex = 0;
+		for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+			if (nums[fastIndex] != val) {
+				nums[slowIndex] = nums[fastIndex];
+				slowIndex++;
+			}
+		}
+		return slowIndex;
+	}
+}
