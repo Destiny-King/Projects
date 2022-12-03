7eb811585058c6cd4a38244e6d5e3a3d975ffb16
Destiny-King <2681283464@qq.com>
2022-12-03 10:48:36 +0800

    Add SpiralMatrix.

--- Java_Algorithm/Array/src/com/wym/algorithm/MainAlgorithm.java
+++ Java_Algorithm/Array/src/com/wym/algorithm/MainAlgorithm.java
@@ -1,6 +1,6 @@
 package com.wym.algorithm;
 
-import com.wym.algorithm.minsubarrayLen.MinSubArrayLen;
+import com.wym.algorithm.spiralmatrix.SpiralMatrix;
 
 public class MainAlgorithm {
 
@@ -28,10 +28,21 @@ public class MainAlgorithm {
 //		}
 
 		//4、长度最小的子数组
-		MinSubArrayLen minSubArrayLen = new MinSubArrayLen();
-		int[] nums = {2, 3, 1, 2, 4, 3};
-		int s = 7;
-		int ret = minSubArrayLen.minSubArrayLen(nums, s);
-		System.out.println(ret);
+//		MinSubArrayLen minSubArrayLen = new MinSubArrayLen();
+//		int[] nums = {2, 3, 1, 2, 4, 3};
+//		int s = 7;
+//		int ret = minSubArrayLen.minSubArrayLen(nums, s);
+//		System.out.println(ret);
+
+		//5、螺旋矩阵II
+		SpiralMatrix spiralMatrix = new SpiralMatrix();
+		int n = 4;
+		int[][] res = spiralMatrix.generateMatrix(n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(res[i][j] + "\t");
+			}
+			System.out.println();
+		}
 	}
 }
