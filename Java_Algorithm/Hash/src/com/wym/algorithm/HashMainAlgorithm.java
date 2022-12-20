91a7f1dc3118776e9a7da780c630253f41042558
Destiny-King <2681283464@qq.com>
2022-12-20 09:43:40 +0800

    Add Intersection.

--- Java_Algorithm/Hash/src/com/wym/algorithm/HashMainAlgorithm.java
+++ Java_Algorithm/Hash/src/com/wym/algorithm/HashMainAlgorithm.java
@@ -1,14 +1,24 @@
 package com.wym.algorithm;
 
-import com.wym.algorithm.isAnagram.IsAnagram;
+import com.wym.algorithm.intersection.Intersection;
 
 public class HashMainAlgorithm {
 
 	public static void main(String[] args) {
-		IsAnagram isAnagram = new IsAnagram();
-		String s = "anagram";
-		String t = "nagaram";
-		boolean anagram = isAnagram.isAnagram(s, t);
-		System.out.println(anagram);
+		//1、有效的字母异位词
+//		IsAnagram isAnagram = new IsAnagram();
+//		String s = "anagram";
+//		String t = "nagaram";
+//		boolean anagram = isAnagram.isAnagram(s, t);
+//		System.out.println(anagram);
+
+		//2、两个数组的交集
+		Intersection intersection = new Intersection();
+		int[] nums1 = {4, 9, 5};
+		int[] nums2 = {9, 4, 9, 8, 4};
+		int[] res = intersection.intersection(nums1, nums2);
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(res[i]);
+		}
 	}
 }
---
+++ Java_Algorithm/Hash/src/com/wym/algorithm/intersection/Intersection.java
@@ -0,0 +1,24 @@
+package com.wym.algorithm.intersection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Intersection {
+
+	public int[] intersection(int[] nums1, int[] nums2) {
+		if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+			return new int[0];
+		}
+		Set<Integer> set1 = new HashSet<>();
+		Set<Integer> resSet = new HashSet<>();
+		for (int i : nums1) {
+			set1.add(i);
+		}
+		for (int i : nums2) {
+			if (set1.contains(i)) {
+				resSet.add(i);
+			}
+		}
+		return resSet.stream().mapToInt(x -> x).toArray();
+	}
+}
