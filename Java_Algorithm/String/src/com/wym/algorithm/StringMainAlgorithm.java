a9abddaf29a930117afe0f862b860bc3364fe4c2
Destiny-King <2681283464@qq.com>
2023-01-11 09:54:18 +0800

    Add ReplaceSpace.

--- Java_Algorithm/String/src/com/wym/algorithm/StringMainAlgorithm.java
+++ Java_Algorithm/String/src/com/wym/algorithm/StringMainAlgorithm.java
@@ -1,20 +1,27 @@
 package com.wym.algorithm;
 
-import com.wym.algorithm.reverseString.ReverseString;
+import com.wym.algorithm.replaceSpace.ReplaceSpace;
 
 public class StringMainAlgorithm {
 	public static void main(String[] args) {
-		ReverseString reverseString = new ReverseString();
-		char[] s = {'h', 'e', 'l', 'l', 'o'};
-		reverseString.reverseString(s);
-		for (char c : s) {
-			System.out.print(c + ",");
-		}
-		System.out.println();
+		//1、反转字符串
+//		ReverseString reverseString = new ReverseString();
+//		char[] s = {'h', 'e', 'l', 'l', 'o'};
+//		reverseString.reverseString(s);
+//		for (char c : s) {
+//			System.out.print(c + ",");
+//		}
+//		System.out.println();
+//
+//		String str = "abcdefg";
+//		int k = 2;
+//		String reverseStr = reverseString.reverseStr(str, k);
+//		System.out.println(reverseStr);
 
-		String str = "abcdefg";
-		int k = 2;
-		String reverseStr = reverseString.reverseStr(str, k);
-		System.out.println(reverseStr);
+		//2、替换空格
+		ReplaceSpace replaceSpace = new ReplaceSpace();
+		String s = "We are happy.";
+		String str = replaceSpace.replaceSpace(s);
+		System.out.println(str);
 	}
 }
