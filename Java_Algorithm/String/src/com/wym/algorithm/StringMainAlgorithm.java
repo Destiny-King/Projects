7db15b1daecf5773ab74e033ef64205545c8594b
Destiny-King <2681283464@qq.com>
2023-01-12 15:43:54 +0800

    Add ReverseWords.

--- Java_Algorithm/String/src/com/wym/algorithm/StringMainAlgorithm.java
+++ Java_Algorithm/String/src/com/wym/algorithm/StringMainAlgorithm.java
@@ -1,6 +1,6 @@
 package com.wym.algorithm;
 
-import com.wym.algorithm.replaceSpace.ReplaceSpace;
+import com.wym.algorithm.reverseWords.ReverseWords;
 
 public class StringMainAlgorithm {
 	public static void main(String[] args) {
@@ -19,9 +19,15 @@ public class StringMainAlgorithm {
 //		System.out.println(reverseStr);
 
 		//2、替换空格
-		ReplaceSpace replaceSpace = new ReplaceSpace();
-		String s = "We are happy.";
-		String str = replaceSpace.replaceSpace(s);
-		System.out.println(str);
+//		ReplaceSpace replaceSpace = new ReplaceSpace();
+//		String s = "We are happy.";
+//		String str = replaceSpace.replaceSpace(s);
+//		System.out.println(str);
+
+		//3、翻转字符串里的单词
+		ReverseWords reverseWords = new ReverseWords();
+		String s = "the sky is blue";
+		String words = reverseWords.reverseWords(s);
+		System.out.println(words);
 	}
 }
